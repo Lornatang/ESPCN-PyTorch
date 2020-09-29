a9cbe2f30a95dfc2571b9eb1bb39553ebf061b8f
Changyu <liuchangyu1111@gmail.com>
2020-09-29 10:45:12 +0800

    up

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -4,4 +4,5 @@ echo "Start downloading pre training model..."
 wget https://github.com/Lornatang/ESPCN-PyTorch/releases/download/1.0/espcn_2x.pth
 wget https://github.com/Lornatang/ESPCN-PyTorch/releases/download/1.0/espcn_3x.pth
 wget https://github.com/Lornatang/ESPCN-PyTorch/releases/download/1.0/espcn_4x.pth
+wget https://github.com/Lornatang/ESPCN-PyTorch/releases/download/1.0/espcn_8x.pth
 echo "All pre training models have been downloaded!"

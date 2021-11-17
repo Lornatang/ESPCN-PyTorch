e50b1ea62b5a76e3f5d6f52498ca10e371f08d5b
Liu Changyu <liuchangyu1111@gmail.com>
2021-11-17 15:13:25 +0800

    Create model.py

---
+++ model.py
@@ -0,0 +1,51 @@
+# Copyright 2020 Dakewe Biotech Corporation. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+import torch
+from torch import nn
+
+
+class ESPCN(nn.Module):
+    """
+
+    Args:
+        upscale_factor (int): Image magnification factor.
+    """
+
+    def __init__(self, upscale_factor: int) -> None:
+        super(ESPCN, self).__init__()
+
+        # Feature mapping
+        self.feature_maps = nn.Sequential(
+            nn.Conv2d(1, 64, (5, 5), (1, 1), (2, 2)),
+            nn.Tanh(),
+            nn.Conv2d(64, 32, (3, 3), (1, 1), (1, 1)),
+            nn.Tanh(),
+        )
+
+        # Sub-pixel convolution layer
+        self.sub_pixel = nn.Sequential(
+            nn.Conv2d(32, 1 * (upscale_factor ** 2), (3, 3), (1, 1), (1, 1)),
+            nn.PixelShuffle(upscale_factor),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self._forward_impl(x)
+
+    # Support torch.script function.
+    def _forward_impl(self, x: torch.Tensor) -> torch.Tensor:
+        out = self.feature_maps(x)
+        out = self.sub_pixel(out)
+
+        return out

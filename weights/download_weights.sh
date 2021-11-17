0071676dd276ae685dc754121ed9e6e52ead6e1b
Liu Changyu <liuchangyu1111@gmail.com>
2021-11-17 15:34:20 +0800

    Delete weights directory

--- weights/download_weights.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Start downloading pre training model..."
-wget https://github.com/Lornatang/ESPCN-PyTorch/releases/download/1.0/espcn_2x.pth
-wget https://github.com/Lornatang/ESPCN-PyTorch/releases/download/1.0/espcn_3x.pth
-wget https://github.com/Lornatang/ESPCN-PyTorch/releases/download/1.0/espcn_4x.pth
-wget https://github.com/Lornatang/ESPCN-PyTorch/releases/download/1.0/espcn_8x.pth
-echo "All pre training models have been downloaded!"

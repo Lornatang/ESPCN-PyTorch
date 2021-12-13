87d8efb00dcbad48d509c89344e38dd96b88c9e9
Liu Changyu <liuchangyu1111@gmail.com>
2021-12-13 17:05:40 +0800

    Update run.py

--- scripts/run.py
+++ scripts/run.py
@@ -7,8 +7,8 @@ os.system("python3 ./prepare_dataset.py --inputs_dir ../data/T91/original --outp
 os.system("python3 ./split_train_valid_dataset.py --images_dir ../data/T91/ESPCN")
 
 # Create LMDB database file
-os.system("python3 ./create_lmdb_database.py --images_dir ../data/T91/ESPCN/train --lmdb_path ../data/train_lmdb/ESPCN/T91_HR_lmdb --upscale_factor 1")
-os.system("python3 ./create_lmdb_database.py --images_dir ../data/T91/ESPCN/train --lmdb_path ../data/train_lmdb/ESPCN/T91_LRbicx3_lmdb --upscale_factor 3")
+os.system("python3 ./create_lmdb_dataset.py --images_dir ../data/T91/ESPCN/train --lmdb_path ../data/train_lmdb/ESPCN/T91_HR_lmdb --upscale_factor 1")
+os.system("python3 ./create_lmdb_dataset.py --images_dir ../data/T91/ESPCN/train --lmdb_path ../data/train_lmdb/ESPCN/T91_LRbicx3_lmdb --upscale_factor 3")
 
-os.system("python3 ./create_lmdb_database.py --images_dir ../data/T91/ESPCN/valid --lmdb_path ../data/valid_lmdb/ESPCN/T91_HR_lmdb --upscale_factor 1")
-os.system("python3 ./create_lmdb_database.py --images_dir ../data/T91/ESPCN/valid --lmdb_path ../data/valid_lmdb/ESPCN/T91_LRbicx3_lmdb --upscale_factor 3")
+os.system("python3 ./create_lmdb_dataset.py --images_dir ../data/T91/ESPCN/valid --lmdb_path ../data/valid_lmdb/ESPCN/T91_HR_lmdb --upscale_factor 1")
+os.system("python3 ./create_lmdb_dataset.py --images_dir ../data/T91/ESPCN/valid --lmdb_path ../data/valid_lmdb/ESPCN/T91_LRbicx3_lmdb --upscale_factor 3")

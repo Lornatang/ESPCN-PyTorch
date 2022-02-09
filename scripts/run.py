d95436d6482d441e7971c50f9e8e8b182bcbf29c
Lorna <liuchangyu1111@gmail.com>
2022-02-09 09:13:58 +0800

    Update run.py

--- scripts/run.py
+++ scripts/run.py
@@ -1,7 +1,7 @@
 import os
 
 # Prepare dataset
-os.system("python ./prepare_dataset.py --images_dir ../data/T91/original --output_dir ../data/T91/ESPCN --image_size 51 --step 39")
+os.system("python ./prepare_dataset.py --images_dir ../data/T91/original --output_dir ../data/T91/ESPCN/train --image_size 51 --step 39")
 
 # Split train and valid
 os.system("python ./split_train_valid_dataset.py --train_images_dir ../data/T91/ESPCN/train --valid_images_dir ../data/T91/ESPCN/valid --valid_samples_ratio 0.1")

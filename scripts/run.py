663dd72ce13cc8495715c4789e54fd432f270505
Lorna <liuchangyu1111@gmail.com>
2022-02-09 09:10:31 +0800

    Update run.py

--- scripts/run.py
+++ scripts/run.py
@@ -1,14 +1,14 @@
 import os
 
 # Prepare dataset
-os.system("python3 ./prepare_dataset.py --inputs_dir ../data/T91/original --output_dir ../data/T91/ESPCN")
+os.system("python ./prepare_dataset.py --images_dir ../data/T91/original --output_dir ../data/T91/ESPCN --image_size 51 --step 39")
 
 # Split train and valid
-os.system("python3 ./split_train_valid_dataset.py --images_dir ../data/T91/ESPCN")
+os.system("python ./split_train_valid_dataset.py --train_images_dir ../data/T91/ESPCN/train --valid_images_dir ../data/T91/ESPCN/valid --valid_samples_ratio 0.1")
 
 # Create LMDB database file
-os.system("python3 ./create_lmdb_dataset.py --images_dir ../data/T91/ESPCN/train --lmdb_path ../data/train_lmdb/ESPCN/T91_HR_lmdb --upscale_factor 1")
-os.system("python3 ./create_lmdb_dataset.py --images_dir ../data/T91/ESPCN/train --lmdb_path ../data/train_lmdb/ESPCN/T91_LRbicx3_lmdb --upscale_factor 3")
+os.system("python ./create_lmdb_dataset.py --images_dir ../data/T91/ESPCN/train --lmdb_path ../data/train_lmdb/ESPCN/T91_HR_lmdb --upscale_factor 1")
+os.system("python ./create_lmdb_dataset.py --images_dir ../data/T91/ESPCN/train --lmdb_path ../data/train_lmdb/ESPCN/T91_LRbicx3_lmdb --upscale_factor 3")
 
-os.system("python3 ./create_lmdb_dataset.py --images_dir ../data/T91/ESPCN/valid --lmdb_path ../data/valid_lmdb/ESPCN/T91_HR_lmdb --upscale_factor 1")
-os.system("python3 ./create_lmdb_dataset.py --images_dir ../data/T91/ESPCN/valid --lmdb_path ../data/valid_lmdb/ESPCN/T91_LRbicx3_lmdb --upscale_factor 3")
+os.system("python ./create_lmdb_dataset.py --images_dir ../data/T91/ESPCN/valid --lmdb_path ../data/valid_lmdb/ESPCN/T91_HR_lmdb --upscale_factor 1")
+os.system("python ./create_lmdb_dataset.py --images_dir ../data/T91/ESPCN/valid --lmdb_path ../data/valid_lmdb/ESPCN/T91_LRbicx3_lmdb --upscale_factor 3")
